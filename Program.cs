05d437b6f410ca4f40f7f82b8a5c9952da99506d
= <=>
2021-08-09 17:46:04 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OLa Wagner");
+        }
+    }
+}
